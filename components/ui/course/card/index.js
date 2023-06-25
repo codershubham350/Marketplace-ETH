dc93094a713194fda4b605304b2bf2cf80fc7c12
codershubham350 <shubhamsaxena350@gmail.com>
2023-06-25 15:30:34 +0530

    order modal and card styling

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -1,33 +1,56 @@
 // Array.from({length:4}) -> created 4 undefined items
 import Image from "next/legacy/image";
 import Link from "next/link";
+import { useState } from "react";
 
-const Card = ({ course }) => {
+const Card = ({ course, Footer }) => {
+  const [descriptionLength, setDescriptionLength] = useState(70);
+  const [showReadMore, setShowReadMore] = useState(true);
+
+  const handleDescriptionLength = (course) => {
+    setDescriptionLength(course?.description?.length);
+    setShowReadMore(!showReadMore);
+  };
   return (
     <>
       <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
         <div className="flex h-full">
-          <div className="flex h-full w-52">
+          <div className="flex-1 next-image-wrapper">
             <Image
               className="object-cover"
               src={course?.coverImage}
               alt={course?.title}
               width={"200"}
               height={"230"}
-              layout="fixed"
+              layout="responsive"
             />
           </div>
-          <div className="p-8">
+          <div className="p-8 pb-4 flex-2" style={{ height: "266px" }}>
             <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
               Case study
             </div>
             <Link
               href={`/courses/${course?.slug}`}
-              className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+              className="h-12 block mt-1 text-lg leading-tight font-medium text-black hover:underline"
             >
               {course?.title}
             </Link>
-            <p className="mt-2 text-gray-500">{course?.description}</p>
+            <p className="mt-2 text-gray-500">
+              {course?.description.substring(0, descriptionLength)}
+              {showReadMore && (
+                <span style={{ cursor: "pointer" }}>
+                  <strong className="pl-2 text-blue-400">...</strong>
+                  <strong
+                    className=" text-blue-400"
+                    style={{ textDecoration: "underline" }}
+                    onClick={(course) => handleDescriptionLength(course)}
+                  >
+                    Read More!
+                  </strong>
+                </span>
+              )}
+            </p>
+            {Footer && <Footer />}
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      flex: {
+        2: "2 2 0%",
+      },
       maxWidth: {
         "8xl": "1920px",
       },
